2e7c5eaa8627e05b73a96048c7108e2e0f088121
Rael <emilyekos@gmail.com>
2025-05-30 21:24:10 +0900

    UI: sharedStyle 공용 컨테이너 UI 적용 #2

--- eatmoji/src/pages/signup/index.tsx
+++ eatmoji/src/pages/signup/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import sharedStyle from "@/styles/shared.module.css";
 
 export default function Main() {
   
@@ -10,7 +11,7 @@ export default function Main() {
         <meta property="og:description" content="이모지 기반 감성 메뉴 추천 서비스" />
         <meta property="og:image" content="/favicon_logo.png" />
       </Head>
-      <div className="flex flex-col justify-center items-center gap-[39px] w-[411px] h-[847px] pt-[100px] px-[65px] flex-shrink-0 bg-[#FFFFFF]">
+      <div className={`${sharedStyle.sharedContainer} gap-[39px]`}>
         <img className="w-[180px] h-[145px] shrink-0" src="/favicon_logo.png" alt="Logo" />
         회원가입 페이지입니다.
       </div>
