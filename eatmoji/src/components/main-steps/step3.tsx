b1f0dde4e9c862d6ceb5f24aa2fa06aa72438be5
Rael <emilyekos@gmail.com>
2025-05-30 21:45:00 +0900

    feat: Main의 step3 컴포넌트 분리 #2

---
+++ eatmoji/src/components/main-steps/step3.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import sharedStyle from "@/styles/shared.module.css";
+
+export default function Step3() {
+  
+  return (
+    <>
+      <Head>
+        <title>Eatmoji😆</title>
+        <meta property="og:title" content="Eatmoji" />
+        <meta property="og:description" content="이모지 기반 감성 메뉴 추천 서비스" />
+        <meta property="og:image" content="/favicon_logo.png" />
+      </Head>
+      <div className={`${sharedStyle.sharedContainer} gap-[39px]`}>
+        <img className="w-[180px] h-[145px] shrink-0" src="/favicon_logo.png" alt="Logo" />
+        step3입니다.
+      </div>
+    </>
+  );
+}
