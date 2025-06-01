cee6d7fce3f1cc397d4a97dfc88fe6c14a0ac0c4
Rael <emilyekos@gmail.com>
2025-06-02 04:07:30 +0900

    feat: 로딩중일때 화면 생성 및 UI 파일 분리 #4

--- eatmoji/src/components/main-steps/step3.tsx
+++ eatmoji/src/components/main-steps/step3.tsx
@@ -1,7 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
 import sharedStyle from "@/styles/shared.module.css";
+import style from "./step3.module.css";
 
-export default function Step3({answer2, generateResult, goToStep} : {answer2: string, generateResult: () => void, goToStep: (stepNumber: number) => void}) {
+export default function Step3({result, goToStep} : {result: string | null, goToStep: (stepNumber: number) => void}) {
+
+  if (result === null) {
+    // 로딩 중일 때 보여줄 화면
+    return (
+      <div className={`${sharedStyle.sharedContainer} ${style.loadingContainer}`}>
+        <div className={style.loader}></div>
+      </div>
+    );
+  }
   
   return (
     <>
@@ -11,18 +22,26 @@ export default function Step3({answer2, generateResult, goToStep} : {answer2: st
         <meta property="og:description" content="이모지 기반 감성 메뉴 추천 서비스" />
         <meta property="og:image" content="/favicon_logo.png" />
       </Head>
-      <div className={`${sharedStyle.sharedContainer} relative gap-[39px]`}>
+      <div className={`${sharedStyle.sharedContainer} ${style.container}`}>
         <button
-          className="absolute top-[50px] left-[40px] w-[90px] h-[30px] text-gray-800 font-inter text-sm font-medium rounded-xl bg-[#f5eee0] hover:bg-[#e8dfcf] transition-all duration-200 shadow-[0_1px_4px_rgba(0,0,0,0.1)] hover:shadow-[0_2px_8px_rgba(0,0,0,0.15)] focus:outline-none focus:ring-2 focus:ring-[#d1c8b0]"
+          className={style.backButton}
           onClick={() => goToStep(1)}
         >
           처음으로
         </button>
-        <div className="flex flex-col items-center gap-[20px]">
-          <h1 className="text-2xl font-bold text-gray-800 text-center w-[250px]">Menu</h1>
-          <img className="w-[180px] h-[145px] shrink-0" src="/favicon_logo.png" alt="Logo" />
+        <div className={style.content}>
+          <h1 className={style.title}>{result}</h1>
+          <img className={style.image} src="/favicon_logo.png" alt="Logo" />
         </div>
-        <p className="text-sm font-normal text-gray-800 text-center w-[270px]">따뜻한 국물은 언제나 위로가 되지. 미소 된장국은 그 자체로도 건강하고, 가볍게 기분 전환하기에 딱이야.</p>
+        <p className={style.description}>따뜻한 국물은 언제나 위로가 되지. 미소 된장국은 그 자체로도 건강하고, 가볍게 기분 전환하기에 딱이야.</p>
+        <div className={style.buttonContainer}>
+          <button className={style.menulinkButton}>
+            만들러가기
+          </button>
+          <button className={style.menulinkButton}>
+            먹으러가기
+          </button>
+        </div>
       </div>
     </>
   );
