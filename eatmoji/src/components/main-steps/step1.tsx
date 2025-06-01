afaa708eabb3ebc61e05446d5c9800eb70ca2ef0
Rael <emilyekos@gmail.com>
2025-06-02 03:06:19 +0900

    UI: step1 UI파일 분리 #4

--- eatmoji/src/components/main-steps/step1.tsx
+++ eatmoji/src/components/main-steps/step1.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import sharedStyle from "@/styles/shared.module.css";
+import style from "./step1.module.css";
 
 export default function Step1({nextStep, setAnswer1} : {nextStep: () => void, setAnswer1: (answer: string) => void}) {
     const handleClick = (answer: string) => {
@@ -15,13 +16,13 @@ export default function Step1({nextStep, setAnswer1} : {nextStep: () => void, se
         <meta property="og:description" content="이모지 기반 감성 메뉴 추천 서비스" />
         <meta property="og:image" content="/favicon_logo.png" />
       </Head>
-      <div className={`${sharedStyle.sharedContainer} gap-[103px]`}>
-        <h1 className="text-2xl font-bold text-gray-800  text-center w-[250px]">오늘 하루, 어땠어?</h1>
-        <div className="flex flex-col items-center gap-[20px]">
+      <div className={`${sharedStyle.sharedContainer} ${style.container}`}>
+        <h1 className={style.title}>오늘 하루, 어땠어?</h1>
+        <div className={style.buttonsWrapper}>
             {["기쁨", "슬픔", "화남", "편안", "아픔"].map((emotion) => (
             <button
                 key={emotion}
-                className="w-[280px] h-[60px] px-4 py-2 text-gray-800 font-inter text-[20px] font-semibold rounded-2xl bg-[#f5eee0] hover:bg-[#e8dfcf] transition-all duration-200 shadow-[0_2px_6px_rgba(0,0,0,0.1)] hover:shadow-[0_4px_12px_rgba(0,0,0,0.2)] focus:outline-none focus:ring-2 focus:ring-[#d1c8b0]"
+                className={style.button}
                 onClick={() => handleClick(emotion)}
             >
                 {emotion}
