6f889732543b715cbe9e13bda79204b3f88ac616
Rael <emilyekos@gmail.com>
2025-06-03 02:48:27 +0900

    feat: 먹으러가기 클릭 시 지역 선택 모달창 open #4

--- eatmoji/src/components/main-steps/step3.tsx
+++ eatmoji/src/components/main-steps/step3.tsx
@@ -2,8 +2,23 @@
 import Head from "next/head";
 import sharedStyle from "@/styles/shared.module.css";
 import style from "./step3.module.css";
+import { useState } from "react";
+import { addressOptions, districts } from "@/dummy/dummyRegion";
 
 export default function Step3({result, goToStep} : {result: string | null, goToStep: (stepNumber: number) => void}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedCity, setSelectedCity] = useState("");
+  const [selectedDistrict, setSelectedDistrict] = useState("");
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  const handleCityChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCity(e.target.value);
+    setSelectedDistrict("");
+  }
+
+  const districtOptions = selectedCity ? districts[selectedCity] || [] : [];
 
   if (result === null) {
     // 로딩 중일 때 보여줄 화면
@@ -39,9 +54,52 @@ export default function Step3({result, goToStep} : {result: string | null, goToS
           <button className={style.menulinkButton}>
             만들러가기
           </button>
-          <button className={style.menulinkButton}>
+          <button className={style.menulinkButton} onClick={openModal}>
             먹으러가기
           </button>
+          {isOpen && (
+            <div className={style.modalOverlay} onClick={closeModal}>
+              <div className={style.modalContent} onClick={(e) => e.stopPropagation()}>
+                <h1 className={style.modalTitle}>지역을 선택해주세요</h1>
+                <label className={style.modalLabel}>
+                  <span>시: </span>
+                  <select 
+                    className={style.selectBox}
+                    value={selectedCity} 
+                    onChange={handleCityChange}
+                  >
+                    <option value="">--선택--</option>
+                    {addressOptions.map((city) => (
+                      <option key={city} value={city}>
+                        {city}
+                      </option>
+                    ))}
+                  </select>
+                </label>
+
+                <label className={style.modalLabel}>
+                  <span>구: </span>
+                  <select
+                    className={style.selectBox}
+                    value={selectedDistrict}
+                    onChange={(e) => setSelectedDistrict(e.target.value)}
+                    disabled={!selectedCity}
+                  >
+                    <option value="">--선택--</option>
+                    {districtOptions.map((district) => (
+                        <option key={district} value={district}>
+                          {district}
+                        </option>
+                      ))}
+                  </select>
+                </label>
+                <div className={style.modalButtons}>
+                  <button className={`${style.modalButton} ${style.cancel}`} onClick={closeModal}>닫기</button>
+                  <button className={style.modalButton} onClick={goToEat}>보러가기</button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
