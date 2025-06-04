72e658d23e625099b94eae064b934c77b43d10b5
Rael <emilyekos@gmail.com>
2025-06-05 04:43:03 +0900

    feat: Step4에 answer1~3 파라미터로 추가 #11

--- eatmoji/src/pages/mypage/profile/personalInfo/index.tsx
+++ eatmoji/src/pages/mypage/profile/personalInfo/index.tsx
@@ -27,7 +27,7 @@ export default function PersonalInfo() {
             case 3:
                 return <Step3 goToStep={goToStep} nextStep={nextStep} setAnswer3={setAnswer3}/>;
             case 4:
-                return <Step4 goToStep={goToStep} setAnswer4={setAnswer4}/>;
+                return <Step4 goToStep={goToStep} setAnswer4={setAnswer4} answer1={answer1} answer2={answer2} answer3={answer3}/>;
             default:
                 return null;
         }
