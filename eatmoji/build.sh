86887d5a3e03ccec4b208b3b41f29c26b7895a1e
Rael <emilyekos@gmail.com>
2025-06-07 23:59:15 +0900

    chore: deploy eatmoji from team repo to personal fork for Vercel

--- eatmoji/build.sh
+++ eatmoji/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-cd ../
-mkdir output
-cp -R ./Eatmoji_FE/* ./output
-cp -R ./output ./Eatmoji_FE/
+
+# 현재 위치: EATMOJI_FE/eatmoji
+mkdir -p ../output
+
+# eatmoji 디렉토리의 내용만 복사
+rsync -av --exclude=node_modules --exclude=.next --exclude=output --exclude=build.sh ./ ../output
