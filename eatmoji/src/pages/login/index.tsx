8fe5f9e2f74f021c20a7e5604f34f4dd6fc060e3
Rael <emilyekos@gmail.com>
2025-06-09 00:54:44 +0900

    feat: 로그인 폼 제출 로직 구현 및 토큰 저장 처리 #15

--- eatmoji/src/pages/login/index.tsx
+++ eatmoji/src/pages/login/index.tsx
@@ -4,6 +4,8 @@ import sharedStyle from "@/styles/shared.module.css";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import style from "./index.module.css";
+import { useAuthStore } from "@/store/auth";
+import { loginRequest } from "@/lib/api/auth";
 
 export default function Login() {
   const router = useRouter();
@@ -17,6 +19,22 @@ export default function Login() {
 
   const goToSignUp = () => {
     router.push("/signup");
+  };
+
+  const handleLoginSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const data = await loginRequest(loginForm.email, loginForm.password);
+      console.log("로그인 성공:", data);
+      const { accessToken, refreshToken } = data;
+      useAuthStore.getState().login(accessToken, refreshToken); // 로그인 성공 후 상태 업데이트
+      router.push("/mypage/profile/personalInfo");
+      alert("로그인 성공! 다음으로 추천에 사용될 개인정보를 입력해주세요.🤗");
+    }
+    catch (error) {
+      console.error("로그인 실패:", error);
+      alert("로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.");
+    }
   };
 
   return (
@@ -34,7 +52,7 @@ export default function Login() {
           <h2 className={style.title}>로그인</h2>
           <p className={style.subtitle}>감성 이모지를 통한 메뉴 추천, 지금 시작해보세요!</p>
 
-          <form className={style.form}>
+          <form className={style.form} onSubmit={handleLoginSubmit}>
             <div>
               <label htmlFor="email" className={style.inputLabel}>
                 이메일
