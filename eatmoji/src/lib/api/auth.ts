f3beb2a7a9f2a84588c9da97d708305b9238be14
Rael <emilyekos@gmail.com>
2025-06-09 00:55:01 +0900

    feat: 회원가입 요청 함수 signupRequest 구현 #15

--- eatmoji/src/lib/api/auth.ts
+++ eatmoji/src/lib/api/auth.ts
@@ -16,3 +16,20 @@ export async function loginRequest(email: string, password: string) {
 
     return res.json();
 }
+
+export async function signupRequest(email: string, password: string) {
+    const res = await fetch(`${BASE_URL}/auth/signup`, {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+    });
+    
+    if (!res.ok) {
+        const errorData = await res.json();
+        throw new Error(errorData.message || "회원가입에 실패했습니다.");
+    }
+    
+    return res.json();
+}
