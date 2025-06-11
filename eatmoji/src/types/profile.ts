1d35c5a107e82d1f43d6eb77608a352a27bb3f7c
Rael <emilyekos@gmail.com>
2025-06-12 03:50:53 +0900

    fix: UserProfile 관련 api 변경으로 인한 interface 수정 #18

--- eatmoji/src/types/profile.ts
+++ eatmoji/src/types/profile.ts
@@ -1,22 +1,3 @@
-export interface Preference {
-  category: {
-    add: string[];
-    remove: string[];
-  };
-  flavor: {
-    add: string[];
-    remove: string[];
-  };
-  disease: {
-    add: string[];
-    remove: string[];
-  };
-  allergy: {
-    add: string[];
-    remove: string[];
-  };
-}
-
 export interface UserProfileData {
   category: string[];
   flavor: string[];
