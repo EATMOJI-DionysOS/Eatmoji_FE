c79411b50e5e98100c987549d3c1cd773c7bdd72
Rael <emilyekos@gmail.com>
2025-06-13 03:40:50 +0900

    feat: 추천 기록 상세페이지 좋아요 api 구현 #20

---
+++ eatmoji/src/lib/api/like.ts
@@ -0,0 +1,24 @@
+import { useTokenStore } from "@/store/tokenStore";
+
+const BASE_URL = process.env.NEXT_PUBLIC_URL_SERVER;
+
+export async function toggleLikeHistoryItem(id: string): Promise<boolean> {
+  const response = await fetch(`${BASE_URL}/history/${id}/like`, {
+    method: 'PATCH',
+    headers: {
+      'accept': '*/*',
+      'Authorization': `Bearer ${useTokenStore.getState().accessToken}`,
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to update like status for history item ${id}`);
+  }
+
+  const text = await response.text();
+
+  if (text.includes('true')) return true;
+  if (text.includes('false')) return false;
+
+  throw new Error('Unexpected response from server');
+}
